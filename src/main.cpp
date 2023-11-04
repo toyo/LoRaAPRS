5759423305efe3c378d7a8bb06498010991da5a9
FUJIURA Toyonori <toyokun@gmail.com>
2023-11-04 22:08:55 +0900

    Add boot message.

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,17 @@ OLEDTask OLED(GPS);
 void setup() {
   Serial.begin(115200);
 
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_EXT0:
+      Serial.println("Wakeup caused by external signal using RTC_IO");
+      break;
+    default:
+      Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+      break;
+  }
+
   c.begin();
 
 #if defined(XPOWERS_CHIP_AXP192)
